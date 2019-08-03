3a006edc4bd33f0b174841b992748ad0875c2dc5
Akshatha.Prabhu <akshatha.prabhu@target.com>
2019-08-03 14:17:42 +0530

    about stacks

--- src/main/a_3_Stack.java
+++ src/main/a_3_Stack.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.io.BufferedReader;
 import java.util.*;
 
 /**
@@ -26,5 +27,46 @@ public class a_3_Stack {
         }
 
         System.out.println("Popping the stack : " + stack.pop());
+
+        System.out.println("________________________________________________\n");
+
+        // code to take inout from user and push into a stack
+        System.out.println("Give total size of stack:  ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        System.out.println("Input the number: ");
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> numbers = new Stack<>();
+        for (int i = 1; i <= num; i++) {
+            numbers.push(sc.nextInt());
+        }
+        System.out.println(numbers);
+
+        System.out.println("________________________________________________\n");
+
+        System.out.println(numbers.peek());  //returns element on top of stack but DOES NOT REMOVE it
+        System.out.println(numbers);
+
+
+        System.out.println(numbers.pop());  //returns element on top of stack but REMOVES it
+        System.out.println(numbers);
+
+        System.out.println("Adding an element");
+        numbers.add(34);                    //Adding an element
+        System.out.println(numbers);
+
+        System.out.println("Removing 3rd element");
+        numbers.remove(2);                  //REMOVING the 3rd element
+        System.out.println(numbers);
+
+        System.out.println("Adding an element in the 5th index");
+        numbers.add(5, 66);                //ADDING an element in given position of the linkedList
+        System.out.println(numbers);
+
+        System.out.println("Replacing the element in the 5th index");
+        numbers.set(5, 56);               //REPLACE an element in given position of the linkedList
+        System.out.println(numbers);
+
     }
 }
